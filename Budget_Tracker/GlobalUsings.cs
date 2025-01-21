ee601022905b628a949f7fc6d7188344eddbada6
ZyadAlessawi <zyadalessawi@gmail.com>
2025-01-21 10:23:55 +0200

    Delete Budget_Tracker directory

--- Budget_Tracker/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Budget_Tracker.Data;
-global using Budget_Tracker.PageModels;
-global using Budget_Tracker.Pages;
-global using Budget_Tracker.Services;
-global using Budget_Tracker.Utilities;
-global using Fonts;
